189580f7a1aaecbd599ba49883ba28b9440ca299
joemitc <joel.mitchell.student@gmail.com>
2025-01-09 15:59:15 -0800

    Test Update 4

--- Test Suites/PLATO Website Suite.ts
+++ Test Suites/PLATO Website Suite.ts
@@ -10,264 +10,264 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
-   <testSuiteGuid>508db94d-1c7c-4f16-bac6-b8de816c9156</testSuiteGuid>
+   <testSuiteGuid>fa42c711-4226-44ac-85bb-29a6cabdb20b</testSuiteGuid>
    <testCaseLink>
-      <guid>33450493-53d9-4012-aa37-2887194a5c00</guid>
+      <guid>9e6844a5-4599-48e8-acb5-29652f25da38</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/1 Navigation/Navigate to IT and Business Consulting page</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>1df0f6fa-4313-4cf6-8452-1e63abd57183</guid>
+      <guid>175557a2-41d9-4ab1-8823-4922aad8d4c0</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/1 Navigation/Navigate to Services page</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>fe26703c-2d57-4388-ade6-73178a7928b5</guid>
+      <guid>17ba6b8d-4603-430c-8338-a116a7dcf0bc</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/1 Navigation/Navigate to Testing Services page</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>acdf949f-841e-46a3-b391-252fa7d82508</guid>
+      <guid>17b38826-ffbc-4b57-9bcf-09019106adea</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/1 Navigation/Navigate to Work with Us form - IT and Business Consulting</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>2542136c-de8a-4f25-b876-5c7b53c5013f</guid>
+      <guid>27acd900-1cde-4039-b337-29ca4dd15caf</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/2 Testing Services/Validate Testing Services Image weblinks</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>bf57aec7-24e7-4552-b73d-8eb21f381d06</guid>
+      <guid>823daf75-3824-4acf-8e44-053d857bc6f9</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/2 Testing Services/Validate Testing Services Images Display</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>a35098fe-cb97-41f7-be3d-ad49e733fb45</guid>
+      <guid>823b4afa-0447-44a8-97e5-1f17b09be376</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/2 Testing Services/Validate Testing Services Weblinks</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>bb8eed61-6d22-46b2-97ca-13c7938d1a16</guid>
+      <guid>1268c0c0-950d-4c3e-9fa9-f2159065fff1</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/2 Testing Services/Verify Test Services header text displays</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>c082dd61-c080-4126-86c8-156889984931</guid>
+      <guid>1fc367f4-4d94-4f40-928d-3fb72670fbe2</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/3 IT and Business/Validate IT and Business Consulting images display</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>c6c4f835-14d7-43fe-a179-07f958a05805</guid>
+      <guid>5e9f205f-5e70-475f-bdbf-40f146ad6525</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/3 IT and Business/Validate IT and Business Consulting weblinks</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>c4c1151f-abc9-4043-95ab-757d75e292b0</guid>
+      <guid>fcdff8f3-7a9c-43a0-b50f-51a0d79d06eb</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/3 IT and Business/Verify IT and Business Consulting header text displays</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>f99f5add-f35d-4306-8095-37200e8fcdc4</guid>
+      <guid>b325612f-1f00-43fe-89bf-543cdf016e9c</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/3 IT and Business/Verify Required Sections appear on the IT and Business Page</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>65dfb89a-84c6-4bd7-8644-b2001b8a7de9</guid>
+      <guid>ba6f4cec-e6ca-4e00-a94e-1996fbda2454</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/4 Services/Services Work With Us Form/Flamingo Scripts/Verify submitted form information is saved in Flamingo</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>7f32cbc6-dec2-4ffe-abdc-4518eade3cc7</guid>
+      <guid>477c8874-a074-4e07-ad43-bc670d8502db</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/4 Services/Services Work With Us Form/Validate Company field accepts letters</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>dcb2cb62-3ede-4c3c-a9e5-d75836734eb4</guid>
+      <guid>d1dbc0b0-f113-46c7-9a0d-96d411a7391f</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/4 Services/Services Work With Us Form/Validate Company field does not accept special characters</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>86873a51-21f7-44a0-a27d-4690d0b6356c</guid>
+      <guid>ba3d507e-ce7b-4d75-8f0c-f60f9844afda</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/4 Services/Services Work With Us Form/Validate Location accepts a user selection</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>02ba5b06-c57b-4286-a1d2-0ef987976536</guid>
+      <guid>f42b3801-1758-40f6-a487-25a6fb78daf4</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/4 Services/Services Work With Us Form/Validate manditory fields must be filled in to submit the form</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>516e99d2-a5fc-4bd8-9b64-8c828ae4211b</guid>
+      <guid>f297d572-abeb-48ab-91dc-8c735048e441</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/4 Services/Services Work With Us Form/Validate name fields accepts up to 100 characters</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>2a40c8ca-213d-4471-b7b9-cc7652664741</guid>
+      <guid>241a9d04-1cf6-49bd-aedd-3c8aeea1af9c</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/4 Services/Services Work With Us Form/Validate name fields on Services page accept letters</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>1d1eca7a-e55f-42c5-ad8f-e7d721aa5996</guid>
+      <guid>23449200-4006-4eac-ba88-dce4dcfea4ac</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/4 Services/Services Work With Us Form/Validate phone field accepts up to 12 characters</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>43c8c756-fef2-4b1f-aaa9-9dc9a36b5617</guid>
+      <guid>1ef0b321-8b4e-45d0-88c4-d0800240a75f</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/4 Services/Services Work With Us Form/Validate phone field on Services page accepts numbers</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>bb375935-eb33-4994-b3a7-d4802e7a9503</guid>
+      <guid>7c34bf98-5b81-4303-a323-fa728de69452</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/4 Services/Services Work With Us Form/Validate phone field on Services page does not accept letters</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>f0bb6ad7-d8ea-4b36-8a28-b7bae38adcc1</guid>
+      <guid>fc65dfd8-48a4-483d-b030-5d4eb75037d8</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/4 Services/Services Work With Us Form/Validate phone field on Services page does not accept mixed values</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>5bfdb3f5-3347-4df3-b3b6-ebf46e068571</guid>
+      <guid>4c0ceb30-17e1-4513-8f63-862e7a95d26b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/4 Services/Services Work With Us Form/Validate Tell Us About Your Project field accepts letters and numbers</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>043fc3c4-5e7c-4ec1-972d-8ec99cbfaed5</guid>
+      <guid>a621d192-deb4-4b4e-aba8-42618b5e48ff</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/4 Services/Services Work With Us Form/Validate Tell Us about Your Project field accepts up to 500 characters</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>531c9efb-c653-4498-bddf-4c51aea18abe</guid>
+      <guid>f2b891ae-4607-483f-94fe-6cc3b0c2f80b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/4 Services/Services Work With Us Form/Verify budget dropdown contains correct budget ranges</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>d4f3e935-272a-470b-95e7-09a706a75746</guid>
+      <guid>200e470b-2b88-45bc-8865-696a1b46ad41</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/4 Services/Services Work With Us Form/Verify fields can be auto populated</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>0343147f-24d2-47d1-a85f-1aee982dfd04</guid>
+      <guid>82736510-04f4-4c72-a168-07327b3a67c4</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/4 Services/Services Work With Us Form/Verify Location dropdown contains all PLATO office locations</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>485d2eba-06ad-4ca2-a12c-f0466473c2e5</guid>
+      <guid>3038590d-b493-4692-82aa-a5cdcc314970</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/4 Services/Services Work With Us Form/Verify manditory fields are marked with an asterisk</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>f8b4274c-d023-4769-a7b9-45d4e78ac247</guid>
+      <guid>c3c0eece-313f-4a13-aeaf-bcdcde4a05f5</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/4 Services/Services Work With Us Form/Verify timeline dropdown includes correct dates</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>0f0465cf-1bcf-4be2-b3ed-d556bf14b14e</guid>
+      <guid>4a70e76f-14b1-42ee-bbc2-38843bf15f8a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/4 Services/Validate Services Image weblinks</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>0fae8898-80e0-4628-b36a-10c0445ae051</guid>
+      <guid>7788d726-636c-433f-815e-c0eb389f589f</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/4 Services/Validate Services Images Display</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>45e04dd7-8287-4279-a0af-28a6ebbaa11f</guid>
+      <guid>d2878114-35af-4b28-b425-21bfc051762f</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/4 Services/Validate Services weblinks</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>d73b4e71-3433-4b65-abf1-df58c52768b8</guid>
+      <guid>4cc25bbc-420a-45f1-8bad-2e693eca73cf</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/PLATO Testing website/4 Services/Verify Services header text displays</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>a46627ad-8e00-4e33-a675-2bf0fcf504c9</guid>
+      <guid>a130dfa4-3d32-4894-99ac-0c4998203f6a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/PLATO Testing website/Verify PLATO Branding on IT and Business page</testCaseId>
+      <testCaseId>Test Cases/PLATO Testing website/5 PLATO Branding/Verify PLATO Branding on IT and Business page</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>16f2336f-3d03-4c4b-934c-ff41a5c2cc38</guid>
+      <guid>29c801ea-7029-4bf3-b206-38e88ef4e40e</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/PLATO Testing website/Verify PLATO Branding on Services page</testCaseId>
+      <testCaseId>Test Cases/PLATO Testing website/5 PLATO Branding/Verify PLATO Branding on Services page</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>f82b4b28-37a6-4e35-90c9-753c7b43d49a</guid>
+      <guid>52fcc051-2a98-45cb-934a-0888dabbeb42</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/PLATO Testing website/Verify PLATO Branding on Testing Services page</testCaseId>
+      <testCaseId>Test Cases/PLATO Testing website/5 PLATO Branding/Verify PLATO Branding on Testing Services page</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
